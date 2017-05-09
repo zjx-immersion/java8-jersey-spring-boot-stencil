68635a2b7d49d4b819ca527d9c4ad1fab194e811
zjx-immersion <jianxin_home@163.com>
2017-05-10 04:06:43 +0800

    [Jason]upgraded the vewsion of spring boot and added the start up process test demo

---
+++ src/main/java/com/thoughtworks/gaia/JerseyConfig.java
@@ -0,0 +1,87 @@
+package com.thoughtworks.gaia;
+
+import com.thoughtworks.gaia.common.filter.CORSFilter;
+import io.swagger.jaxrs.config.BeanConfig;
+import io.swagger.jaxrs.listing.ApiListingResource;
+import io.swagger.jaxrs.listing.SwaggerSerializers;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.message.filtering.EntityFilteringFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+
+//@Configuration
+@Component
+public class JerseyConfig extends ResourceConfig {
+    private static final String[] MODULE_PACKAGES = new String[]{
+        "com.thoughtworks.gaia.product.endpoint",
+    };
+
+    @Value("${spring.jersey.application-path:/}")
+    private String apiPath;
+
+    public JerseyConfig() {
+        // common packages
+//        this.packages(
+//            "com.thoughtworks.gaia.common.exception.handler"
+//        );
+
+        this.packages(
+                "com.thoughtworks.gaia"
+        );
+
+        // module packages
+//        this.packages(
+//            MODULE_PACKAGES
+//        );
+
+//        register(CORSFilter.class);
+//        this.registerClasses(
+//            ApiListingResource.class,
+//            SwaggerSerializers.class,
+//            EntityFilteringFeature.class,
+//            JacksonFeature.class
+//        );
+
+    }
+
+    @PostConstruct
+    public void init() {
+        // Register components where DI is needed
+        this.configureSwagger();
+    }
+
+//    protected void initSwaggerBeanConfig() {
+//        BeanConfig beanConfig = new BeanConfig();
+//        beanConfig.setVersion("1.0.0");
+//        beanConfig.setTitle("Jersey API Doc");
+//        beanConfig.setSchemes(new String[]{"http"});
+//        beanConfig.setHost("localhost:8080");
+//        beanConfig.setBasePath("/gaia/rest");
+//        beanConfig.setResourcePackage(Joiner.on(",").join(MODULE_PACKAGES));
+//        beanConfig.setScan(true);
+//    }
+
+    private void configureSwagger() {
+        System.out.println("init bean config in Jersey config:" + apiPath);
+        // Available at localhost:port/swagger.json
+        this.register(ApiListingResource.class);
+        this.register(SwaggerSerializers.class);
+
+        BeanConfig config = new BeanConfig();
+//        config.setConfigId("springboot-jersey-swagger-docker-example");
+        config.setTitle("Spring Boot + Jersey + Swagger  Example");
+        config.setVersion("v1");
+//		config.setHost("localhost:8080");
+        config.setContact("Leo Jiang");
+        config.setSchemes(new String[] { "http", "https"});
+        config.setBasePath(apiPath);
+        config.setResourcePackage("com.example.demo");
+        config.setPrettyPrint(true);
+        config.setScan(true);
+    }
+}
---
+++ src/main/java/com/thoughtworks/gaia/common/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.thoughtworks.gaia.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by jxzhong on 2017/5/9.
+ */
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.demo"))
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/");
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Swagger 2")
+                .description("更多Spring Boot相关文章请关注：http://blog.didispace.com/")
+                .termsOfServiceUrl("http://www.baidu.com/")
+                .contact("程序猿Leo Jiang")
+                .version("1.0.1.0")
+                .build();
+    }
+}
